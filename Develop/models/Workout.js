67c7bf01e2f2d11ba0349c6baf726035d2c5514a
Julian Secomb <hoodooprojects@gmail.com>
2020-06-06 20:47:37 -0700

    mostly working app

---
+++ Develop/models/Workout.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const moment = require("moment");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: moment().format('MM/DD/YYYY')
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+      },
+      name: {
+        type: String,
+      },
+      duration: {
+        type: Number
+      },
+      weight: {
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+      sets: {
+        type: Number
+      },
+      distance: {
+        type: Number
+      }      
+    }
+  ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
---
+++ Develop/public/index.js
@@ -0,0 +1,19 @@
+init();
+
+async function init() {
+  if (location.search.split("=")[1] === undefined) {
+    const workout = await API.getLastWorkout();
+    if (workout) {
+      location.search = "?id=" + workout._id;
+    } else {
+      document.querySelector("#continue-btn").classList.add("d-none")
+    }
+  }
+}
+
+async function deleteAll() {
+  const res = await fetch("/api/workouts", {
+    method: "DELETE"
+  });
+}
+
